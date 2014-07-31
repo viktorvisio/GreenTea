792669905ce5266a155310c54ded8bcefdfd968c
viktorvisio <kuzma_819@hotmail.com>
2014-07-31 17:42:31 +0200

    Matrix.h reworked

--- src/math/Matrix.h
+++ src/math/Matrix.h
@@ -12,6 +12,8 @@
 //#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <fstream>
+#include "../structures/Exception.h"
 //typedef (double)(func*)();
 
 namespace GT{
@@ -25,6 +27,12 @@ private:
 	int columns;
 public:
 
+	Matrix(){
+		data = NULL;
+		rows = -1;
+		columns = -1;
+	}
+
 	Matrix(int x, int y){
 		rows = x;
 		columns = y;
@@ -36,7 +44,18 @@ public:
 		}
 	}
 
+	Matrix(const Matrix<T>& b){
+		init(b.rows,b.columns);
+		T** d = b.data;
+		for(int n = 0; n < rows; n++){
+			for(int m = 0; m < columns; m++){
+				data[n][m] += d[n][m];
+			}
+		}
+	}
+
 	~Matrix(){
+		//printf("Dealokacia\n");
 		for(int n = 0; n < rows; n++){
 			delete data[n];
 		}
@@ -65,28 +84,26 @@ public:
 		return data[x];
 	}
 
-	T& getVal(int x, int y){
+	T& getVal(int x, int y) const{
 		return data[x][y];
 	}
 
-	void setVal(int x, int y,const T& val){
-		data[x][y] = *val;
+	void setVal(int x, int y, T val){
+		data[x][y] = val;
 	}
 
 	void mult(T scalar){
 		for(int m = 0; m < rows; m++)
 			for(int n = 0; n < columns; n++)
-				data[n][m] *= scalar;
-	}
-
-	Matrix<T>* multSimple(Matrix<T>* source){
-
-		return this;
+				data[m][n] *= scalar;
 	}
 
 	Matrix<T>* multTrans(Matrix* source, Matrix* destination){
 		if(columns != source->columns){
-			return NULL;
+
+			throw Exception("Matrix multTrans: Dimension missmatch!");
+
+			//return NULL;
 		}
 
 		if(destination == NULL){
@@ -116,10 +133,18 @@ public:
 		data[x][y] *= val;
 	}
 
-	Matrix<T>* mult(Matrix* source, Matrix* destination){
+	void setAll(T val){
+		for(int n = 0; n < rows; n++){
+			for(int m = 0; m < columns; m++){
+				data[n][m] = val;
+			}
+		}
+	}
+
+	Matrix<T>* mult(Matrix* source, Matrix* destination) const{
 
 		if(columns != source->rows){
-			return NULL;
+			throw Exception("Matrix mult: Dimension missmatch!");
 		}
 
 		if(destination == NULL){
@@ -146,6 +171,112 @@ public:
 		return destination;
 	}
 
+	Matrix<T>* mult(Matrix* source, Matrix* destination, unsigned int offset_rows, unsigned int offset_columns) const{
+
+			if(columns != source->rows){
+				throw Exception("Matrix mult: Dimension missmatch!");
+			}
+
+			if(destination == NULL){
+				destination = new Matrix(rows, source->columns);
+			}
+			else{
+				 if(destination->getRows() < rows || destination->getColumns() < source->columns){
+					 printf("Error:\tTarget Matrix(%d,%d), Destination Matrix(%d,%d)\n", rows, columns, destination->getRows(), destination->getColumns());
+					 throw Exception("Matrix mult: Destination matrix dimension missmatch!");
+				 }
+			}
+			//	source
+			T** sd = source->data;
+			T** dd = destination->data;
+
+			int byc = source->columns;
+
+			double val = 0;
+
+			for(int m = 0; m < rows; m++){
+				for(int n = 0; n < byc; n++){
+					val = 0;
+					for(int ay = 0; ay < columns; ay++)
+						val += getVal(m, ay) * sd[ay][n];
+
+					dd[offset_rows+m][offset_columns+n] = val;
+				}
+			}
+
+			return destination;
+	}
+
+	Matrix<T> mult(const Matrix& source, unsigned int offset_rows = 0, unsigned int offset_columns = 0) const{
+
+				if(columns != source.rows){
+					printf("Error:\tA(%d,%d), B(%d,%d)\n", rows, columns, source.getRows(), source.getColumns());
+					throw Exception("Matrix mult: Dimension missmatch!");
+				}
+
+				Matrix<T> destination(rows, source.columns);
+				//	source
+				T** sd = source.data;
+				T** dd = destination.data;
+
+				int byc = source.columns;
+
+				double val = 0;
+
+				for(int m = 0; m < rows; m++){
+					for(int n = 0; n < byc; n++){
+						val = 0;
+						for(int ay = 0; ay < columns; ay++)
+							val += getVal(m, ay) * sd[ay][n];
+
+						dd[offset_rows+m][offset_columns+n] = val;
+					}
+				}
+
+				return destination;
+	}
+
+	/**
+	 * Multiplikacia. Offset urcuje od kade sa zacne zapisovat do cielovej matice.
+	 * Parameter func je funkcia, ktora je vykonana nad kazdou vypocitanou hodnotou. Je to na zrychlenie.
+	 */
+	Matrix<T>* multLambda(Matrix* source, Matrix* destination, unsigned int offset_rows, unsigned int offset_columns, T (*func)(T)) const{
+
+				if(columns != source->rows){
+					throw Exception("Matrix mult: Dimension missmatch!");
+				}
+
+				if(destination == NULL){
+					destination = new Matrix(rows, source->columns);
+				}
+				else{
+					if(destination->getRows() < rows || destination->getColumns() < source->columns){
+						printf("Error:\tTarget Matrix(%d,%d), Destination Matrix(%d,%d)\n", rows, columns, destination->getRows(), destination->getColumns());
+						throw Exception("Matrix mult: Destination matrix dimension missmatch!");
+					}
+				}
+				//	source
+				T** sd = source->data;
+				T** dd = destination->data;
+
+				int byc = source->columns;
+				double val = 0;
+
+				for(int m = 0; m < rows; m++){
+					for(int n = 0; n < byc; n++){
+						val = 0;
+						for(int ay = 0; ay < columns; ay++)
+							val += getVal(m, ay) * sd[ay][n];
+
+						dd[offset_rows+m][offset_columns+n] = func(val);
+					}
+				}
+
+				return destination;
+	}
+
+
+
 	void printS(int fd){
 		for(int n = 0; n < rows; n++){
 			for(int m = 0; m < columns; m++){
@@ -156,6 +287,7 @@ public:
 			printf("\n");
 
 		}
+		printf("\n");
 	}
 
 	Matrix<T>* lambda(T (*func)(T)){
@@ -166,6 +298,38 @@ public:
 		}
 
 		return this;
+	}
+
+	static Matrix<T> lambda(const Matrix<T> &source, T (*func)(T)){
+		Matrix<T> r(source.getRows(), source.getColumns());
+		for(int n = 0; n < source.getRows(); n++){
+			for(int m = 0; m < source.getColumns(); m++){
+				r.data[n][m] = func(source.data[n][m]);
+			}
+		}
+
+		return r;
+	}
+
+	Matrix<T>* lambda(int from_row, int from_column, T (*func)(T)){
+			for(int n = from_row; n < rows; n++){
+				for(int m = from_column; m < columns; m++){
+					data[n][m] = func(data[n][m]);
+				}
+			}
+
+			return this;
+	}
+
+	void lambda(int from_row, int from_column, T (*func)(T), Matrix<T>* dest){
+
+		T** d = dest->data;
+
+				for(int n = from_row; n < rows; n++){
+					for(int m = from_column; m < columns; m++){
+						d[n][m] = func(data[n][m]);
+					}
+				}
 	}
 
 	void sub(Matrix<T>* b){
@@ -216,11 +380,11 @@ public:
 		return mat;
 	}
 
-	const int& getRows(){
+	const int& getRows() const{
 		return rows;
 	}
 
-	const int& getColumns(){
+	const int& getColumns() const{
 		return columns;
 	}
 
@@ -233,23 +397,200 @@ public:
 
 	}
 
-	/*
-	Matrix<T>& operator-(Matrix<T> &b){
+	Matrix<T> row(int p){
+		Matrix<float> r(1,columns);
 
-		//if(b.xc != xc && b.yc != yc)
-		//	return NULL;
+		for(int n =0; n < columns; n++){
+			r.setVal(0,n, data[p][n]);
+		}
 
-		Matrix<T> nt = Matrix<T>(xc,yc);
+		return r;
+	}
 
-		for(int n = 0; n < xc; n++){
-			for(int m = 0; m < yc; m++){
-				//nt.setVal(n,m, &(data[n][m] - b.getVal(n,m)));
+	Matrix<T> row(int p, int from){
+			Matrix<float> r(1,columns-from);
+
+			int t = 0;
+			for(int n = from; n < columns; n++){
+				r.setVal(0,t, data[p][n]);
+				++t;
+			}
+
+			return r;
+	}
+
+
+	Matrix<T> cloneMatrix(){
+			Matrix<float> r(rows,columns);
+
+			for(int m =0; m < rows; m++)
+			for(int n =0; n < columns; n++){
+				r.setVal(m,n, data[m][n]);
+			}
+
+			return r;
+	}
+
+
+	Matrix<T> operator-(const Matrix<T> &b){
+
+		if(b.rows != rows || b.columns != columns)
+			throw Exception("Matrices don't have same size.");
+
+		Matrix<T> nt = Matrix<T>(rows,columns);
+
+		for(int n = 0; n < rows; n++){
+			for(int m = 0; m < columns; m++){
+				nt.setVal(n,m, (data[n][m] - b.data[n][m]));
 			}
 		}
 
 		return nt;
 	}
-	*/
+
+	Matrix<T> operator*(const Matrix<T> &b) const{
+		return mult(b);
+	}
+
+	Matrix<T> dotMult(const Matrix<T> &b){
+		if(rows != b.rows || columns != b.columns){
+			printf("Error:\tA(%d,%d), B(%d,%d)\n", rows, columns, b.getRows(), b.getColumns());
+			throw Exception("Error:\t dotMult Matrices are not same!");
+		}
+
+		Matrix<T> r(rows,columns);
+		T** d = b.data;
+		T** dest = r.data;
+
+		for(int n = 0; n < rows; n++){
+			for(int m = 0; m < columns; m++){
+				dest[n][m] = data[n][m]*d[n][m];
+			}
+		}
+
+		return r;
+	}
+
+	Matrix<T> operator!(void) const{
+
+		Matrix<T> r(columns, rows);
+		T** dest = r.data;
+
+		for(int n = 0; n < rows; n++){
+			for(int m = 0; m < columns; m++){
+				dest[m][n] = data[n][m];
+			}
+		}
+		return r;
+	}
+
+	void operator+=(const Matrix<T> &b){
+
+		if(rows != b.rows || columns != b.columns){
+
+			if(/*rows == -1 && columns == -1 && */data == NULL){
+				init(b.rows, b.columns);
+				setAll(0);
+			}
+			else{
+
+				printf("Error:\tA(%d,%d), B(%d,%d)\n", rows, columns, b.getRows(), b.getColumns());
+				throw Exception("Error:\t += Matrices are not same!");
+			}
+		}
+
+		T** d = b.data;
+
+		for(int n = 0; n < rows; n++){
+			for(int m = 0; m < columns; m++){
+				data[n][m] += d[n][m];
+			}
+		}
+
+	}
+
+	void operator-=(const Matrix<T> &b){
+
+			if(rows != b.rows || columns != b.columns){
+
+				if(/*rows == -1 && columns == -1 && */data == NULL){
+					init(b.rows, b.columns);
+					setAll(0);
+				}
+				else{
+
+					printf("Error:\tA(%d,%d), B(%d,%d)\n", rows, columns, b.getRows(), b.getColumns());
+					throw Exception("Error:\t += Matrices are not same!");
+				}
+			}
+
+			T** d = b.data;
+
+			for(int n = 0; n < rows; n++){
+				for(int m = 0; m < columns; m++){
+					data[n][m] -= d[n][m];
+				}
+			}
+
+	}
+
+
+
+	void operator=(const Matrix<T> &b){
+		init(b.rows,b.columns);
+
+		T** d = b.data;
+		for(int n = 0; n < rows; n++){
+			for(int m = 0; m < columns; m++){
+						data[n][m] = d[n][m];
+			}
+		}
+	}
+
+
+	void printInfo(const char* name){
+
+		printf("Matrix %s(%d,%d)\n",name,rows,columns);
+
+	}
+
+	void trimColumns(int howmuch){
+		columns -= howmuch;
+	}
+
+
+	void initFromFile(std::string filename){
+
+		std::string name;
+
+		std::ifstream myInFile;
+		//std::ifstream::in
+		myInFile.open(filename.c_str(), std::ifstream::in);
+
+		int p = 0;
+		char buffer[1024];
+
+		while(!myInFile.eof() && p < 20)
+		{
+			myInFile.getline(buffer,1024);
+			p++;
+		     //   myInFile >> name;
+		//   //     a[x][y] = value;
+		        printf("N: %s\n", buffer);
+		}
+
+		myInFile.close();
+
+
+	}
+
+	void setRow(int r, T t[]){
+
+		for(int m = 0; m < columns; m++){
+			data[r][m] = t[m];
+		}
+	}
+
 };
 
 };
